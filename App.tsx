53c00f6674949eed27d067ffcf080680ce2e9f94
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-15 12:07:26 -0500

    feat: :sparkles: Refactor App component to use SingInScreen and prepare for font loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import { Text, View, ActivityIndicator } from "react-native";
 import LoginScreen from "./src/screens/LoginScreen";
 import SingInScreen from "./src/screens/SingInScreen";
+// import * as Font from "expo-font";
 
 export default function App() {
+    // const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    // useEffect(() => {
+    //   async function loadFonts() {
+    //     await Font.loadAsync({
+    //       // Nombres personalizados para las fuentes
+    //       "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    //       "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    //     });
+    //     setFontsLoaded(true);
+    //   }
+    //   loadFonts();
+    // }, []);
+  
+    // if (!fontsLoaded) {
+    //   return (
+    //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    //       <ActivityIndicator size="large" />
+    //     </View>
+    //   );
+    // }
     // return <LoginScreen />;
     return <SingInScreen />;
   }
