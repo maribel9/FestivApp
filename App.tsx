d053adb3eb151c7b32d7c3aa0555a483619f48c9
BrayanBJ27 <xavi_braian@hotmail.com>
2025-01-15 11:57:22 -0500

    feat: :sparkles: Replace LoginScreen with SingInScreen in App component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import LoginScreen from "./src/screens/LoginScreen";
+import SingInScreen from "./src/screens/SingInScreen";
 
 export default function App() {
-    return <LoginScreen />;
+    // return <LoginScreen />;
+    return <SingInScreen />;
   }
